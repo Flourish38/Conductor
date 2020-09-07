9cd0883be01637e64c94b9ad6a3b41d25e091dec
Ben <brsandvik@gmail.com>
2020-09-07 15:06:39 -0700

    DEAR GOD that was annoying to troubleshoot. actually made restart be on "restart" instead of "end"

--- src/main/java/listeners/RestartCommand.java
+++ src/main/java/listeners/RestartCommand.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class RestartCommand extends ParameterCommand {
     public RestartCommand() {
-        super("end");
+        super("restart");
     }
 
     @Override
